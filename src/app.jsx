08acdefb4abc0e9539f67c25fe8b6feba3f4f21a
Willy <willy@gdn-commerce.com>
2017-03-24 10:00:06 +0700

    commit(graphql): testing graphql capability

--- src/app.jsx
+++ src/app.jsx
@@ -3,30 +3,29 @@ import '../styles/index.scss';
 import AppNavbar from './components/appnavbar';
 import PokeCard from './components/pokecard';
 import { Card, CardTitle, Navbar, NavItem, Button, Row, Col, Icon } from 'react-materialize';
-import { Axios } from 'axios';
-// Use Axios to fetch v2 api
+import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo'
 
-const axios = new Axios();
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: '//pokeapi-graphiql.herokuapp.com'
+  })
+});
+
 export default class App extends React.Component {
-  getPokeLists() {
-    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=811')
-      .then(function(response) {
-        console.log(response)
-      });
-  }
   constructor() {
     super();
-    this.getPokeLists();
   }
   render() {
     return (
         <div>
-          <AppNavbar></AppNavbar>
-          <div className="container">
-            <div className="row">
-              <PokeCard />
+            <AppNavbar></AppNavbar>
+            <div className="container">
+              <div className="row">
+                <ApolloProvider client={client}>
+                  <PokeCard />
+                </ApolloProvider>
+              </div>
             </div>
-          </div>
         </div>  
     );
   }
--- src/components/pokecard.jsx
+++ src/components/pokecard.jsx
@@ -1,14 +1,95 @@
 import React from 'react';
+import { gql, graphql } from 'react-apollo';
+
+let pokedexOffset = 0;
+pokeListings: [];
+
+const Listings = ( {pokedex, loadMoreEntries}) => {
+  console.log(pokedex);
+  return (<div>
+    <a onClick={loadMoreEntries}>Load</a>
+  </div>);
+}
+
+const POKEDEX_QUERY = gql`
+  query FetchPokes($offset: Int!, $limit: Int!) {
+    pokedex {
+      id
+      pokemon(start: $offset, number: $limit) {
+        edges {
+          node {
+            national_id
+            name
+            types {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+            sprites {
+              edges {
+                node {
+                  modified
+                  image
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ListingsWithData = graphql( POKEDEX_QUERY, {
+  options(props) {
+    return {
+      variables : {
+        offset: pokedexOffset,
+        limit: 10
+      }
+    }
+  },
+  props({ data: {pokedex, fetchMore} }) {
+    return {
+      pokedex,
+      loadMoreEntries() {
+        return fetchMore({
+          variables: {
+            offset: pokedexOffset+=10
+          },
+          updateQuery: (previousResult, {fetchMoreResult}) => {
+            if(!fetchMoreResult.pokedex) {
+              console.log("asd", fetchMoreResult)
+              return previousResult; 
+            } else {
+              console.log("fetch new", previousResult.pokedex.pokemon.edges, fetchMoreResult.pokedex.pokemon.edges)
+              let newDexs = previousResult.pokedex.pokemon
+              newDexs = newDexs.concat(fetchMoreResult.pokedex.pokemon);
+              console.log(newDexs);
+              return Object.assign({}, previousResult, {
+                pokedex: newDexs
+              });
+            }
+            
+            
+          }
+        });
+      }
+    }
+  }
+})(Listings)
 
 class Pokecard extends React.Component {
   constructor() {
     super();
-    this.state = { someKey: 'someValue' };
   }
 
   render() {
     return (
       <div className="col s12 m4 l3">
+        <ListingsWithData />
         <div className="card hide-on-small-only waves-effect waves-block waves-orange">
           <div className="card-image">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" />
@@ -27,10 +108,6 @@ class Pokecard extends React.Component {
         </div>
       </div>
     );
-  }
-
-  componentDidMount() {
-    this.setState({ someKey: 'otherValue' });
   }
 }
 
