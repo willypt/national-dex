9633551968827368376867031b5f47983aff8271
Willy <willy@gdn-commerce.com>
2017-03-25 07:58:16 +0700

    commit(typefilter modal): clickable type filters

--- src/components/TypesFilterDiv.jsx
+++ src/components/TypesFilterDiv.jsx
@@ -5,10 +5,27 @@ import PokeListStore from '../stores/PokeListStore';
 const pokemonAllTypes = ["Normal", "Fire", "Fighting", "Water", "Flying", "Grass", "Poison", "Electric",
   "Ground", "Psychic", "Rock", "Ice", "Bug", "Dragon", "Ghost", "Dark", "Steel", "Fairy", "???"];
 
+function getState() {
+  return {
+    activeTypes: PokeListStore.getTypes(),
+  }
+}
+
 class TypesFilterDiv extends React.Component {
+  componentDidMount() {
+    PokeListStore.addChangeListener(this._onChange)
+  }
+  componentWillUnmount() {
+    PokeListStore.removeChangeListener(this._onChange)
+  }
+  _onChange() {
+    this.setState(getState());
+  }
   constructor() {
     super();
+    this.state = getState();
     this.render = this.render.bind(this);
+    this._onChange = this._onChange.bind(this);
   }
   instAndOpenModal() {
     var s = $("#type-filter-modal").modal();
@@ -19,10 +36,19 @@ class TypesFilterDiv extends React.Component {
     return (
       <div>
         <a className="dropdown-button btn col s12" href="#type-filter-modal" onClick={this.instAndOpenModal}>Type Filter</a>
-        <div id="type-filter-modal" className="modal bottom-sheet">
+        <div id="type-filter-modal" className="modal">
           <div className="modal-content">
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
+            <h4>Filter Type</h4>
+            <div className="row">
+              {pokemonAllTypes.map(function(type, index) {
+                return (
+                  <p className="col s6" key={index}>
+                    <input type="checkbox" className="" value={type} id={"type" + index}/>
+                    <label htmlFor={"type" + index}>{type}</label>
+                  </p>
+                );
+              })}
+            </div>
           </div>
           <div className="modal-footer">
             <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">CLose</a>
